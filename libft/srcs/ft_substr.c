f4ebc8830b12d9d4d49628305697a8e8ee20ce26
mehrasmeydani <52368993+mehrasmeydani@users.noreply.github.com>
2023-09-06 18:55:09 +0200

    Add files via upload

---
+++ libft/srcs/ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgardesh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 16:03:03 by mgardesh          #+#    #+#             */
+/*   Updated: 2023/09/06 16:27:52 by mgardesh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	lens;
+	size_t	i;
+	char	*out;
+
+	lens = ft_strlen(s);
+	if (start > lens)
+		return (NULL);
+	if (len + start > lens)
+		out = (char *)malloc((lens - start + 1) * (char));
+	else
+		out = (char *)melloc((len + 1) * (char));
+	if (!out)
+		return (out);
+	i = start;
+	while (s[i] && i )
+}

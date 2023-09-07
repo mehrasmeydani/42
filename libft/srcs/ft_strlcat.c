01b9018477241d2afe3a8d231bb863e23ee43a11
mehrasmeydani <52368993+mehrasmeydani@users.noreply.github.com>
2023-09-07 12:15:49 +0200

    Update ft_strlcat.c

--- libft/srcs/ft_strlcat.c
+++ libft/srcs/ft_strlcat.c
@@ -12,21 +12,27 @@
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
+	size_t	dest_len;
+	size_t	src_len;
 	size_t	i;
 
+	dest_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (size < dest_len)
+		return (src_len + size);
 	i = 0;
-	while (src[i] != '\0' && i < size - 1 && size > 0)
+	while (src[i] != '\0' && ((i + dst_len) < size - 1) && size > 0)
 	{
-		dst[i] = src[i];
+		dst[dest_len + i] = src[i];
 		i++;
 	}
-	dst[i] = '\0';
+	dst[dest_len + i] = '\0';
 	while (src[i] != '\0')
 	{
-		dst[i] = '\0';
+		dst[dest_len + i] = '\0';
 		i++;
 	}
-	return (i);
+	return (src_len + dest_len);
 }

96c99bd2f9847e3db477706f75f8b19c184ec47b
Mehras Gardesh Meydani <mgardesh@c4r3p2.42vienna.com>
2023-09-07 15:05:08 +0200

    libft update

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -40,5 +40,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
 char	*ft_strdup(const char *s);
 void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif

da66db7087fcb573a912f771b6ff1c72ffca5ee0
mehrasmeydani <52368993+mehrasmeydani@users.noreply.github.com>
2023-09-07 12:03:41 +0200

    Create ft_strjoin.c

---
+++ libft/srcs/ft_strjoin.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*out:
+
+	out = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * (char));
+	if (!out)
+		return (out);
+	
+}

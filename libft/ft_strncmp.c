aa36b268c701090480b3f9333935142d9e29ccc3
mehrasmeydani <52368993+mehrasmeydani@users.noreply.github.com>
2023-09-11 15:17:43 +0000

    a

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -23,7 +23,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (str1[i] == str2[i] && str1[i] != '\0' && str2[i] != '\0' && (i < n - 1))
+	while (str1[i] == str2[i] && str1[i] != '\0'
+		&& str2[i] != '\0' && (i < n - 1))
 		i++;
 	return (str1[i] - str2[i]);
 }

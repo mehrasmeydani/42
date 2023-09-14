b60cdfb98166dbf7fb31951c77d4333a56c47103
mehrasmeydani <52368993+mehrasmeydani@users.noreply.github.com>
2023-09-14 00:59:52 +0000

    abas agha

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 00:27:29 by codespace         #+#    #+#             */
+/*   Updated: 2023/09/14 00:32:30 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*out;
+
+	out = (t_list *)malloc(sizeof(*out));
+	if (!out)
+		return (NULL);
+	out->content = content;
+	out->next = NULL;
+	return (out);
+}

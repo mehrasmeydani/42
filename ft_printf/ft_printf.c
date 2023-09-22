f7ff7f921f13ac1615a505fb878e519d59c066a4
mehrasmeydani <52368993+mehrasmeydani@users.noreply.github.com>
2023-09-22 12:21:37 +0000

    done with eval

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,18 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 14:38:17 by mgardesh          #+#    #+#             */
-/*   Updated: 2023/09/19 09:36:08 by codespace        ###   ########.fr       */
+/*   Updated: 2023/09/21 16:49:48 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include "stdarg.h"
+ 
+int	ft_printf(const char *s, ...)
+{
+	va_list	input_ptr;
+	char	*input;
+
+	va_start(input_ptr, input);
+	return(1);
+}
--- test/test_jakob/ft_strnstr.c
+++ test/test_jakob/ft_strnstr.c
@@ -3,14 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsadjina <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 19:46:08 by jsadjina          #+#    #+#             */
-/*   Updated: 2023/09/19 12:37:48 by jsadjina         ###   ########.fr       */
+/*   Updated: 2023/09/22 11:07:20 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+//#include "libft.h"
+#include <stddef.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	cnt;
+
+	cnt = 0;
+	while (str[cnt] != '\0')
+		cnt++;
+	return (cnt);
+}
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t				i;
+	const unsigned char	*str1 = s1;
+	const unsigned char	*str2 = s2;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (str1[i] == str2[i] && i < n - 1)
+		i++;
+	return (str1[i] - str2[i]);
+}
+
+char	*ft_1strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if ((!big && len == 0) || (len < ft_strlen(little) && !ft_strlen(big)))
+		return (NULL);
+	if (!*little && big)
+		return ((char *)big);
+	while (big[i] && i < len)
+	{
+		while (big[i + j] && big[i + j] == little[j] && i + j < len)
+			j++;
+		if (!little[j])
+			return ((char *)(big + i));
+		j = 0;
+		i++;
+	}
+	return (NULL);
+}
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
@@ -36,21 +84,15 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	}
 	return (0);
 }
-/*
-#include <string.h>
+
+#include <bsd/string.h>
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-	char big[] = "asdad";
-	char little[] = "as";
-	int len = 2;
-	char *result;
-
-	result = ft_strnstr(big, little, len);
-
-	printf("%s\n", result);
-//	printf("%s", result);
-	return(0);
+    char str1[6] = {288 ,48, -128, 50, 1, 0};
+    char str2[5] = {32, 48, 128, 50, 0};
+    // Test the custom strnstr function against the actual strnstr function
+    printf("%s\n%s\n%s\n",ft_strnstr(str1, str2, 4),ft_1strnstr(str1, str2, 4), strnstr(str1, str2, 4));
+    return 0;
 }
-*/
